66d35eb99cbb766e5ecf0a52858a33f678fc07bf
Vicky <Vicky@DESKTOP-GEQSLSP>
2018-05-17 21:26:17 -0700

    alt sort

--- Practice/src/zoho/alternatesort.java
+++ Practice/src/zoho/alternatesort.java
@@ -1,5 +1,24 @@
 package zoho;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
 public class alternatesort {
 
+	public static void main(String[] args) {
+		int a[]={1, 2, 3, 4, 5, 6, 7};  //7162534
+		Arrays.sort(a);
+		
+		int i=0,j=a.length-1;
+		int b[]=new int [a.length];
+		for(int k=0;k<b.length;k++) {
+			if(k%2==0) { b[k]=a[j]; j--;}
+			else {b[k]=a[i]; i++;}
+		}
+	for(int v:b)	
+	System.out.println(v);	
+	}
 }
